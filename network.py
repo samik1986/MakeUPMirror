40f752c83547149f7d67ab3cc34b7a241060793f
Samik Banerjee <cs12d016@smail.iitm.ac.in>
2017-07-17 10:04:12 +0530

    DATA preparation

--- network.py
+++ network.py
@@ -7,7 +7,7 @@ from keras.models import Model
 from keras.layers import Dense, Dropout, Flatten, merge, UpSampling2D, Reshape, BatchNormalization
 from keras.layers.merge import add
 from keras.layers import Input, TimeDistributed
-from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Conv2D, MaxPooling2D,concatenate
 from keras.optimizers import SGD
 from keras.utils import plot_model
 from IPython.display import SVG
@@ -18,7 +18,7 @@ from tensorflow.python.ops import array_ops
 from scipy.linalg._expm_frechet import vec
 from tensorflow.python.framework import ops
 from tensorflow.python.framework.op_def_library import _Flatten, _IsListValue
-from keras.callbacks import TensorBoard
+from keras.callbacks import TensorBoard, ModelCheckpoint
 from LRN import LRN
 from custom_merge import Discrim
 
@@ -46,7 +46,7 @@ mu_op = np.load('MU_OP.npy')
 
 input_dim =  [100,100,3]
 
-sess = tf.InteractiveSession()
+# sess = tf.InteractiveSession()
 
 def create_network(input_dim):
 
@@ -80,32 +80,52 @@ def create_network(input_dim):
 
     #---Common Path----------
 
-    comb = Discrim()([x_end,y_end])
+    comb = concatenate([x_end,y_end],axis=-1)
     print comb
-    temp = Conv2D(3, (3, 3), activation='relu', padding='same')(comb)
 
-    print temp
+    z = Conv2D(512, (3, 3), activation='relu', padding='same')(comb)
+    # z = Flatten()(z)
+    # z = Dense(512*100*100,activation='relu')(z)
+    # z = Dense(4096, activation='relu')(z)
+    z = Dropout(0.5)(z)
+    # z = Dense(4096,activation='relu')(z)
+    # z = Dense(512*100*100, activation='relu')(z)
+    # z = Reshape((100,100,512))(z)
+    z = Conv2D(512, (3, 3), activation='relu', padding='same')(z)
+    z = Conv2D(256, (3, 3), activation='relu', padding='same')(z)
+    z = Conv2D(128, (3, 3), activation='relu', padding='same')(z)
+    z = Conv2D(64, (3, 3), activation='relu', padding='same')(z)
+    z = Conv2D(32, (3, 3), activation='relu', padding='same')(z)
+
+    op = Conv2D(3, (3, 3), activation='sigmoid', padding='same')(z)
+
+    print op
 
     final = Model(inputs=[input_mu, input_nm],
-                  outputs=temp)
+                  outputs=op)
 
     return final
 
 
 model = create_network(input_dim)
-# print(model.summary())
+print(model.summary())
 # plot_model(model, to_file='model.png')
 
 
-sgd = SGD(lr=0.001, decay=1e-6, momentum=0.7, nesterov=True)
-
+# sgd = SGD(lr=0.001, decay=1e-6, momentum=0.7, nesterov=True)
+adadelta = keras.optimizers.adadelta(lr=0.001,decay=1e-5)
 model.compile(loss='binary_crossentropy',
               optimizer='adadelta')
+filepath="models/ckpt{epoch:02d}.hdf5"
+checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
+
 
 model.fit([mu_ip,nm_ip],
           mu_op,
-          batch_size=10, epochs=5,
-          callbacks=[TensorBoard(log_dir='/tmp/autoencoder')])
+          batch_size=20, epochs=1000,
+          callbacks=[TensorBoard(log_dir='models/'),checkpoint])
+
+
 
 # score = model.evaluate([x_test, x1_test],
 #                        [y_test, y_aux_test],
